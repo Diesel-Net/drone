ff7de45fd88912b0cd05892a477bdc4498d5f2c8
Thomas Daley <tomdaley92@gmail.com>
2021-08-27 19:29:02 -0700

    no longer doing dns lookups

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 git clone --depth=1 --branch=stable git@github.com:Diesel-Net/drone-inventory.git .ansible/inventory
 
-pip install -r .ansible/files/requirements.txt
-
 export ANSIBLE_CONFIG="$(pwd)/.ansible/ansible.cfg"
 ansible-galaxy install -r .ansible/roles/requirements.yaml -p .ansible/roles --force
 ansible-playbook .ansible/deploy.yaml -i .ansible/inventory/production/hosts --vault-id ~/.tokens/master_id --extra-vars "git_repository=drone git_branch=production"
-
-pip uninstall -r .ansible/files/requirements.txt -y
 
 rm -rf .ansible/inventory
