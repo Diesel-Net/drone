5c9663cf36eaea96e768af055ac64d9042aa6bcb
Thomas Daley <tomdaley92@gmail.com>
2022-08-10 17:55:20 -0700

    update to use new IaC

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git clone --depth=1 --branch=stable git@github.com:Diesel-Net/drone-inventory.git .ansible/inventory
 
 export ANSIBLE_CONFIG="$(pwd)/.ansible/ansible.cfg"
-ansible-galaxy install -r .ansible/roles/requirements.yaml -p .ansible/roles --force
-ansible-playbook .ansible/deploy.yaml -i .ansible/inventory/production --extra-vars "repository=drone version=production"
+#ansible-galaxy install -r .ansible/roles/requirements.yaml -p .ansible/roles --force
+ansible-playbook .ansible/deploy.yaml -i .ansible/inventory/production --extra-vars "repository=drone version=prod"
 
 rm -rf .ansible/inventory
