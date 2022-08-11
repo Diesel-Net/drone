0e6ddc5dba86fe54b14590f8b6c4926dba5f7bd0
Thomas Daley <tomdaley92@gmail.com>
2022-08-10 18:20:18 -0700

    remove ansible config export

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 git clone --depth=1 --branch=stable git@github.com:Diesel-Net/drone-inventory.git .ansible/inventories
 
-export ANSIBLE_CONFIG="$(pwd)/.ansible/ansible.cfg"
 ansible-galaxy install -r .ansible/roles/requirements.yaml -p .ansible/roles --force
 ansible-playbook .ansible/deploy.yaml -i .ansible/inventories/prod.yaml --extra-vars "repository=drone version=prod"
 
